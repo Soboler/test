6f02676455950bcee2f6210094b3f2ac589f615b
Soboler <antohasob@mail.ru>
2021-10-05 00:31:55 +0500

    first commit

---
+++ NumberComparison.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interview
+{
+    /* Задача:
+
+    Даны два целых неотрицательных числа, записанных следующим образом. Каждая цифра записывается её 
+    английским названием, первая буква цифры большая, остальные - маленькие.
+
+    Например, число 1293 записывается как "OneTwoNineThree", число 135 как "OneThreeFive", число 0 
+    как "Zero". Ведущие нули в записи отсутствуют (за исключением самого числа 0).
+
+    Нужно сравнить эти два числа (сравнивать их нужно как числа) и вернуть:
+    -1, если первое число меньше, 
+    0, если значения чисел совпадают, 
+    1, если первое число больше.
+
+    Пример использования:
+    var result = new NumberStringsComparer().Compare("OneTwoNineThree", "OneThreeFive");
+ 
+    Для проверки решения необходимо запустить тесты.
+
+    Подсказка:
+    При использовании класса System.Numerics.BigInteger не пройдут тесты TestBig (числа из 200 тысяч цифр).
+    */
+
+    public class NumberStringsComparer
+    {
+        private static readonly string[] digits =
+        {
+            "Zero",
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine",
+        };
+
+        public int Compare(string first, string second)
+        {
+            var firstDigits = ConvertToDigits(first);
+            var secondDigits = ConvertToDigits(second);
+            return CompareListsOfDigits(firstDigits, secondDigits);
+        }
+
+        private List<int> ConvertToDigits(string s)
+        {
+            var result = new List<int>();
+
+            int prevUpper = 0;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (char.IsUpper(s[i]))
+                {
+                    var textDigit = s[prevUpper..i];
+                    prevUpper = i;
+
+                    result.Add(Array.IndexOf(digits, textDigit));
+                }
+                else continue;
+            }
+            string lastDigitt = s.Substring(prevUpper);
+            result.Add(Array.IndexOf(digits, lastDigitt));
+
+            return result;
+        }
+
+        private int CompareListsOfDigits(List<int> first, List<int> second)
+        {
+            if (first.Count > second.Count)
+                return 1;
+            if (first.Count < second.Count)
+                return -1;
+
+            for (int i = 0; i < first.Count; i++)
+            {
+                if (first[i] == second[i])
+                    continue;
+                if (first[i] > second[i])
+                    return 1;
+                if (first[i] < second[i])
+                    return -1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ Tests.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Interview
+{
+    // Если тесты не запускаются через Test Explorer, выполните Clean Solution
+    public class Tests
+    {
+        public static IEnumerable<object[]> TestCases()
+        {
+            yield return new object[] {"Zero", "Zero", 0};
+            yield return new object[] {"One", "Zero", 1};
+            yield return new object[] {"Zero", "One", -1};
+            yield return new object[] {"One", "Two", -1};
+            yield return new object[] {"One", "One", 0};
+            yield return new object[] {"OneTwo", "TwoOne", -1};
+            yield return new object[] {"OneZero", "One", 1};
+            yield return new object[] {"OneEight", "Nine", 1};
+            yield return new object[] {"OneNine", "TwoEight", -1};
+            yield return new object[] {"SixNine", "TwoNine", 1};
+            yield return new object[] {"FiveSixOne", "SixFourOne", -1};
+            yield return new object[] {"OneZero", "OneOne", -1};
+            yield return new object[] {"ThreeTwo", "Four", 1};
+            yield return new object[] {"OneZeroSeven", "OneZeroSix", 1};
+            yield return new object[] {"Eight", "Nine", -1};
+            yield return new object[] {"Four", "Five", -1};
+            yield return new object[] {"Eight", "Seven", 1};
+            yield return new object[] {"OneZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZero", "OneZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZero", -1};
+            yield return new object[] {"OneZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZero", "OneZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroZeroOne", -1};
+            yield return new object[] {"OneThreeTwoFourNineZeroSixFiveEightNine", "OneFourThreeTwoNineZeroSixFiveEightNine", -1};
+            yield return new object[] {"ThreeThreeThree", "TwoTwoTwo", 1};
+            yield return new object[] {"OneThreeTwoFourNineZeroSixFiveEightNine", "OneThreeTwoFourNineZeroSixFiveEightNine", 0};
+            yield return new object[] {"NineOneThreeTwoFourNineSixFiveEight", "OneThreeTwoFourNineZeroSixFiveEightNine", -1};
+            yield return new object[] {"OneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOne", "TwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwo", -1};
+            yield return new object[] {"OneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneTwo", "TwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoOne", -1};
+            yield return new object[] {"OneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOne", "TwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwo", 1};
+            yield return new object[] {"OneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneOneThree", "TwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoZero", 1};
+            yield return new object[] {"TwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwo", "ThreeThreeThreeThreeThreeThreeThreeThreeThreeThreeThreeThreeThreeThreeThree", 1};
+            yield return new object[] {"ThreeThreeThreeThreeThreeThreeThreeThreeThreeThreeThreeThreeThreeThreeThree", "TwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwoTwo", -1};
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public void Test(string first, string second, int expected)
+        {
+            var actual = new NumberStringsComparer().Compare(first, second);
+            Assert.AreEqual(expected, actual);
+        }
+
+        public static IEnumerable<object[]> TestCasesBig()
+        {
+            yield return new object[] {0, 1, -1};
+            yield return new object[] {5, 4, 1};
+            yield return new object[] {7, 7, 0};
+        }
+
+        [TestCaseSource(nameof(TestCasesBig)), Timeout(1000)]
+        public void TestBig1(int lastDigit1, int lastDigit2, int expected)
+        {
+            int len = 200000;
+            var builder = new StringBuilder(5 * len);
+            for (int i = 0; i < len; i++)
+                builder.Append(digits[(i + 1) % 10]);
+            var s = builder.ToString();
+            var first = s + digits[lastDigit1];
+            var second = s + digits[lastDigit2];
+            var actual = new NumberStringsComparer().Compare(first, second);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCaseSource(nameof(TestCasesBig)), Timeout(1000)]
+        public void TestBig2(int lastDigit1, int lastDigit2, int expected)
+        {
+            int len = 200000;
+            var builder = new StringBuilder(5 * len);
+            for (int i = 0; i < len; i++)
+                builder.Append(digits[9]);
+            var s = builder.ToString();
+            var first = s + digits[lastDigit1];
+            var second = s + digits[lastDigit2];
+            var actual = new NumberStringsComparer().Compare(first, second);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test, Timeout(1000)]
+        public void TestBig3()
+        {
+            int len = 200000;
+            var builder = new StringBuilder(5 * len);
+            for (int i = 0; i < len; i++)
+                builder.Append(digits[0]);
+            var s = builder.ToString();
+            var first = digits[3] + s;
+            var second = digits[5];
+            var actual = new NumberStringsComparer().Compare(first, second);
+            Assert.AreEqual(1, actual);
+        }
+
+        private static readonly string[] digits =
+        {
+            "Zero",
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine",
+        };
+    }
+}
